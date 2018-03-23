2cf077c48a24c14ea4eb7bdd3d9d9a69a31a2126
Alex Janckila <alex.janckila@dollarshaveclub.com>
2018-03-22 21:23:29 -0700

    NEXT implement GetSearchJobStatus

--- search.go
+++ search.go
@@ -3,10 +3,12 @@ package sumologic
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 //https://help.sumologic.com/APIs/Search-Job-API/About-the-Search-Job-API#Creating_a_search_job
@@ -15,7 +17,7 @@ import (
 // use ISO 8601 for time ranges
 // Process Flow
 // 1. Request a Search Job - Client.StartSearch(*Search) - query and time range.
-// 2. Response - a search job ID or error
+// 2. Response - a search job ID or error SearchJob
 // 3. Request search status - Client.GetSearchStatus(id int) must be done every 5 in at least
 // 4. Response
 //      - a job status 'gathering results', 'done executing', etc
@@ -31,13 +33,29 @@ type StartSearchRequest struct {
 	TimeZone string `json:"timeZone"`
 }
 
-// SearchJob represents a search job in Sumologic
+// SearchJob represents a search job in Sumologic, returned after starting a search.
 type SearchJob struct {
 	Status  int    `json:"status"`
 	ID      string `json:"id,omitempty"`
 	Code    string `json:"code"`
 	Message string `json:"message"`
 	Cookies []*http.Cookie
+}
+
+// SearchJobStates are the different states a search job can be in.
+var SearchJobStates = map[string]string{
+	"NOT STARTED":            "Search job has not been started yet.",
+	"GATHERING RESULTS":      "Search job is still gathering more results, however results might already be available.",
+	"FORCE PAUSED":           "Query that is paused by the system. It is true only for non-aggregate queries that are paused at the limit of 100k. This limit is dynamic and may vary from customer to customer.",
+	"DONE GATHERING RESULTS": "Search job is done gathering results; the entire specified time range has been covered.",
+	"CANCELED":               "The search job has been canceled.",
+}
+
+// HistogramBucket corresponds to the histogram display in the Sumo Logic interactive analytics API.
+type HistogramBucket struct {
+	Length         int       `json:"length"`
+	Count          int       `json:"count"`
+	StartTimeStamp time.Time `json:"startTimeStamp"`
 }
 
 // StartSearch calls the Sumologic API Search Endpoint.
@@ -83,4 +101,26 @@ func (c *Client) StartSearch(ssr StartSearchRequest) (*SearchJob, error) {
 	default:
 		return nil, fmt.Errorf("unexepected http status code %v", resp.StatusCode)
 	}
+}
+
+// SearchJobStatusRequest is a wrapper for the search job status params.
+type SearchJobStatusRequest struct {
+	SearchJobID string `json:"searchJobId"`
+	Offset      int    `json:"offset"`
+	Limit       int    `json:"limit"`
+}
+
+// SearchJobStatusResponse stores the response from getting a search status.
+type SearchJobStatusResponse struct {
+	State           string             `json:"state"`
+	MessageCount    int                `json:"messageCount"`
+	HistgramBuckets []*HistogramBucket `json:"histogramBuckets"`
+	RecordCount     int                `json:"recordCount"`
+	PendingWarnings []string           `json:"pendingWarnings"`
+	PendingErrors   []string           `json:"pendingErrors`
+}
+
+// GetSearchJobStatus retrieves the status of a running job.
+func (c *Client) GetSearchJobStatus(params SearchJobStatusRequest) (*SearchJobStatusResponse, error) {
+	return nil, errors.New("Not Implemented")
 }
--- search_test.go
+++ search_test.go
@@ -53,3 +53,44 @@ func TestStartSearch(t *testing.T) {
 		return
 	}
 }
+
+func TestGetSearchStatus(t *testing.T) {
+	// req.Header.Set("Cookie", "name=xxxx; count=x")
+	testSearchJob := SearchJob{
+		ID: "testsearchjob",
+	}
+	testSearchJobStatusRequest := SearchJobStatusRequest{
+		SearchJobID: testSearchJob.ID,
+		Offset:      0,
+		Limit:       100,
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if r.Method != "GET" {
+			t.Errorf("Expected ‘GET’ request, got ‘%s’", r.Method)
+		}
+		expectedURL := fmt.Sprintf("/search/jobs/%s", testSearchJob.ID)
+		if r.URL.EscapedPath() != expectedURL {
+			t.Errorf("Expected request to ‘%s’, got ‘%s’", expectedURL, r.URL.EscapedPath())
+		}
+		body, _ := json.Marshal(SearchJobStatusResponse{
+			State: "GATHERING RESULTS",
+		})
+		w.Write(body)
+	}))
+	defer ts.Close()
+
+	c, err := NewClient("accessToken", ts.URL)
+	if err != nil {
+		t.Errorf("NewClient() returned an error: %s", err)
+		return
+	}
+
+	_, err = c.GetSearchJobStatus(testSearchJobStatusRequest)
+	if err != nil {
+		t.Errorf("GetSearchJobStatus() returned an error: %s", err)
+		return
+	}
+
+}
